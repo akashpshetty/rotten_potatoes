6ffa43ab7292c1dae00b6949e577b8c88bec9b28
Akash Shetty <akashpshetty@gmail.com>
2013-10-31 17:38:52 -0700

    Change application.rb config file and deactivate asset precompilation config

--- config/application.rb
+++ config/application.rb
@@ -53,5 +53,8 @@ module Rottenpotatoes
 
     # Assume all model attributes are safe by default
     config.active_record.whitelist_attributes = false
+
+    # Added by Akash
+    config.assets.initialize_on_precompile = false
   end
 end
